398a164ae552cfb544cb2a66abe1a81ed68a6731
Mayank Jain <mayank.jain@coditas.com>
2019-03-27 14:49:31 +0530

    Fixed border mechanism.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -67,21 +67,22 @@ export default {
     // `this` points to the vm instance
     this.todos = [
       {
-        title: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+        title: `1: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
         done: false
       },
       {
-        title: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+        title: `2: Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
 
 The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackha`,
         done: false
       },
       {
-        title: `Contrary to popular belief, Lorem Ipsum is not simply random text.`,
+        title: `3: Contrary to popular belief, Lorem Ipsum is not simply random text.`,
         done: false
       }
     ];
     this.activeTodoIndex = -1;
+    this.activeDraggedItem = -1;
   },
   methods: {
     addEventListener(event) {
@@ -107,6 +108,8 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
       this.todos.splice(index, 1);
     },
     dragStart(which, ev) {
+      this.activeDraggedItem = which;
+
       ev.dataTransfer.setData("Text", this.id);
       ev.dataTransfer.dropEffect = "move";
       this.dragging = which;
@@ -148,15 +151,26 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
         document
           .getElementById("todo-wrapper" + this.activeTodoIndex)
           .classList.remove("borderBottom");
+        document
+          .getElementById("todo-wrapper" + this.activeTodoIndex)
+          .classList.remove("borderTop");
       }
       this.activeTodoIndex = i;
-      document.getElementById("todo-wrapper" + i).classList.add("borderBottom");
+
+      if(this.activeDraggedItem >= i) {
+        document.getElementById("todo-wrapper" + i).classList.add("borderTop");
+      } else {
+        document.getElementById("todo-wrapper" + i).classList.add("borderBottom");
+      }
     },
     dragLeave(ev, i) {
       if (this.activeTodoIndex != i) {
         document
           .getElementById("todo-wrapper" + i)
           .classList.remove("borderBottom");
+        document
+          .getElementById("todo-wrapper" + i)
+          .classList.remove("borderTop");
       }
     },
     dragEnd(ev) {
@@ -165,8 +179,10 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
       for (let i = 0; i < todoItems.length; i++) {
         todoItems[i].setAttribute("draggable", false);
         todoItems[i].classList.remove("borderBottom");
+        todoItems[i].classList.remove("borderTop");
       }
       this.activeTodoIndex = -1;
+      this.activeDraggedItem = -1;
 
       ev.srcElement.style.opacity = "1";
 
@@ -327,8 +343,10 @@ body {
 .draggedItem .todo-item {
   box-shadow: 1px 1px 2px 2px #ccc !important;
 }
-
 .borderBottom {
+  border-bottom: 2px solid darkgray;
+}
+.borderTop {
   border-top: 2px solid darkgray;
 }
 </style>
